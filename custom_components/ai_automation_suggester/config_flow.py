fac6adb644859d8f0f09f2eb9dc4d5a546fd8419
Greg LeBlanc <greg@glsec.us>
2024-11-01 21:54:36 -0400

    Update custom_components/ai_automation_suggester/config_flow.py
    Co-authored-by: Luke Lashley <conway220@gmail.com>

--- custom_components/ai_automation_suggester/config_flow.py
+++ custom_components/ai_automation_suggester/config_flow.py
@@ -105,7 +105,7 @@ class ProviderValidator:
                 "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent",
                 headers=headers,
                 json=test_request,
-                params=param
+                params=params
 
             )
             is_valid = response.status == 200
